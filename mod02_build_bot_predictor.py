dc6835e5eccb350d1ed2dc4371ea3f272ac2f0e2
Malbyte <61992340+Malbyte@users.noreply.github.com>
2026-02-16 23:11:43 -0500

    test

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -12,8 +12,8 @@ def train_model(X, y, seed=seed):
     model = GradientBoostingClassifier(
         learning_rate=0.001,
         n_estimators=1000,
-        max_depth=16,
-        subsample=0.1,
+        max_depth=8,
+        subsample=0.01,
         min_samples_leaf=1,
         random_state=seed
     )
